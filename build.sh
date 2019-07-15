647b54797a15a6f82d1244240f04a6bb09212033
karsa <wanghongtao114@gmail.com>
2019-07-15 15:59:05 +0800

    update gitignore & build

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+rm ./FileServer*
+rm ./*.zip
+
 env GOOS=linux GOARCH=amd64 go build -o FileServer-linux
 env GOOS=windows GOARCH=amd64 go build -o FileServer-windows.exe
 env GOOS=darwin GOARCH=amd64 go build -o FileServer-mac
+
+zip ./linux.zip ./FileServer-linux ./upload.html
+zip ./mac.zip ./FileServer-mac ./upload.html
+zip ./windows.zip ./FileServer-windows.exe ./upload.html
+
+rm ./FileServer*
